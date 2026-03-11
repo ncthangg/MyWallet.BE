c143b27dd39ba1fe616070a1a42b4f7039da54c0
ncthangg <ncthang2206@gmail.com>
2026-03-11 16:17:15 +0700

    update enum

--- MyWallet.Domain/Constants/Enum/Enum.cs
+++ MyWallet.Domain/Constants/Enum/Enum.cs
@@ -1,15 +1,32 @@
 ﻿namespace MyWallet.Domain.Constants.Enum
 {
+    public enum RoleCategory
+    {
+        Admin,
+        User
+    }
+    public enum PaymentStatus
+    {
+        PENDING,
+        SUCCESS,
+        FAILED
+    }
+    public enum AccountProvider
+    {
+        BANK,
+        MOMO,
+        VNPAY,
+        ZALOPAY
+    }
+    public enum QRReceiverType
+    {
+        PERSONAL,
+        GUEST,
+    }
     public enum BankCode
     {
         VCB,
         TPB,
         SCB
-
-    }
-    public enum RoleCategory
-    {
-        Admin,
-        User
     }
 }
